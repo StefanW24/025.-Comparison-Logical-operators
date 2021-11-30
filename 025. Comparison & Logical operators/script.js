38ad3cc3e8d256d1ff7b856cf7c183e890300e0a
StefanW24 <90267126+StefanW24@users.noreply.github.com>
2021-11-30 16:19:52 +0100

    Add files via upload

---
+++ 025. Comparison & Logical operators/script.js
@@ -0,0 +1,42 @@
+const age = 26;
+
+if (age >= 18 && age <= 25) {
+
+    console.log("you get 50% off!");
+
+} else {
+
+    console.log("you are not getting discount");
+
+}
+
+const firstname = "Bram";
+
+if (firstname === "Sarah" || firstname === "Bram") {
+
+    console.log("Free beer!")
+
+} else {
+
+    console.log("Sorry no free beer for you");
+
+}
+
+const totalAmount = 100;
+const totalAmountdrinks = 50
+
+if (totalAmountdrinks > 50) {
+
+    console.log("Free veggie bitterballen");
+
+}
+
+if (totalAmount > 50 && totalAmount < 100) {
+
+    console.log("Free portion of nacho's");
+
+} else if (totalAmount >= 100) {
+
+    console.log("Free bottle of Champagne");
+
+}
